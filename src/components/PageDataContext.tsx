171f525fd95dd025a7fd356639cdaa7c52720f1b
Flemming Hansen <gnimmelf@gmail.com>
2025-03-26 12:08:09 +0100

    Loading fontmatter from vfile

--- src/components/PageDataContext.tsx
+++ src/components/PageDataContext.tsx
@@ -1,14 +1,7 @@
-import {
-  createResource,
-  createContext,
-  JSX,
-  useContext,
-  createEffect,
-  createMemo,
-  Show,
-} from "solid-js";
+import { createResource, createContext, JSX, useContext } from "solid-js";
 import { RouteMatch, useCurrentMatches } from "@solidjs/router";
 import { Resource } from "solid-js/types/server/rendering.js";
+import resolveMdxComponent from "~/utils/resolveMdxComponent";
 
 export type FrontMatter = {
   title: string;
@@ -31,27 +24,19 @@ export function PageDataProvider(props: { children: JSX.Element }) {
       return m[m.length - 1];
     },
     async (match): Promise<PageData> => {
-      console.log(match)
-      return new Promise((resolve) =>
-        setTimeout(() => {
-          resolve({
-            frontmatter: {
-              title: "TEST TITLE",
-              intro: "TEST INTRO",
-            },
-            path: match.path,
-          });
-        }, 10)
-      );
+      const { $component } = match.route.key as { $component: any };
+      const resolved = await resolveMdxComponent($component);
+      const { frontmatter } = resolved;
+      console.log(frontmatter);
+      return {
+        frontmatter,
+        path: match.path
+      };
     },
     { deferStream: true }
   );
 
-  return (
-    <Context.Provider value={pageData}>
-      {props.children}
-    </Context.Provider>
-  );
+  return <Context.Provider value={pageData}>{props.children}</Context.Provider>;
 }
 
 export const usePageData = () => {
